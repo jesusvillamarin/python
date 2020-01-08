fb1ca1150c0698b0a44e672ad58b0500a39c7569
Jesus Auriol Villamarin Ortiz <j.villamarin@outlook.com>
2020-01-08 10:11:28 -0500

    Se agrego Schedule para proximamente crear l proyecto por funciones especificas

--- Platforms_courses/Platzi/projects/web_scrapping/README.md
+++ Platforms_courses/Platzi/projects/web_scrapping/README.md
@@ -7,3 +7,22 @@
         ╚███╔███╔╝███████╗██████╔╝    ███████║╚██████╗██║  ██║██║  ██║██║     ██║     ██║██║ ╚████║╚██████╔╝
          ╚══╝╚══╝ ╚══════╝╚═════╝     ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝     ╚═╝╚═╝  ╚═══╝ ╚═════╝ 
 </span>
+
+To run the main.py script need the following steps
+
+1. virtualenv venv
+2. pip install -r requeriments.txt
+
+> run: python main.py --max-images= number_of_images_to_download
+
+##### Bonus
+
+Run python scripts without the python word
+
+1. Run cmd as administrator
+2. Run the following
+
+        > assoc .py=Python
+        > ftype Python="c:\Anaconda(VERSION)\python.exe" "%1" %*
+
+3. Restart the cmd
--- Platforms_courses/Platzi/projects/web_scrapping/main.py
+++ Platforms_courses/Platzi/projects/web_scrapping/main.py
@@ -3,45 +3,105 @@ from bs4 import BeautifulSoup
 import urllib
 import sys
 import getopt
+import time
+import schedule
+import os
+import shutil
+
 
 URL = 'https://xkcd.com/'
-max_images = 1
+DOWNLOAD_FOLDER = './downloads/'
+MAX_IMAGES = 1
+CLEAR_FOLDER = False
+INDEX_TO_START = 1
 
-def run(max_images):
-    """
-        This method download all image from https://xkcd.com
-    """
-    for i in range(1, max_images+1):
-        response = requests.get(URL + str(i))
-        if response.status_code == 200:
-            soup = BeautifulSoup(markup=response.content, features='html.parser')
-            print(f'The page title: {soup.title.string}')
-            comic_container = soup.find(name='div', attrs= {'id': 'comic'}) # The find method return a bs4.element.Tag
-            img_tag = comic_container.find('img') # the find method is part of bs4 module
-            # print(type(img_tag))
 
-            img_url = 'https:' + img_tag.get('src')
-            img_title = img_tag.get('title')
-            img_name = img_url.split('/')[-1]
-            urllib.request.urlretrieve(img_url, img_name) # To Download the image
+def find_image(markup_content = None):
+    if markup_content is None: raise ValueError('The request response not should be None')
 
-            print(f'The URL for {img_title} is: {img_url}')
+    soup = BeautifulSoup(markup=markup_content, features='html.parser')
 
-        else:
-            print(response.status_code)
+    comic_container = soup.find(name='div', attrs= {'id': 'comic'}) # The find method return a bs4.element.Tag
+    img_tag = comic_container.find('img') # the find method is part of bs4 module
+    # print(type(img_tag))
 
-if __name__ == '__main__':
-    args = sys.argv[1:]
+    img_url = 'https:' + img_tag.get('src')
+    img_title = img_tag.get('title')
+    img_name = img_url.split('/')[-1]
+
+    return img_name, img_url, soup.title.string
+
+def get_argv():
+    argsv = sys.argv[1:]
     try:
-        opts, args = getopt.getopt(args,'', ['max-images='])
+        opts, args = getopt.getopt(argsv,'d', ['max-images=', 'path-folder=', 'clear-folder', 'start-from='])
         for key, value in opts:
             if key == '--max-images':
-                max_images = int(value)
-
+                global MAX_IMAGES
+                MAX_IMAGES = int(value)
+            elif key == '--path-folder':
+                global DOWNLOAD_FOLDER
+                DOWNLOAD_FOLDER = value
+            elif key == '--clear-folder':
+                global CLEAR_FOLDER
+                CLEAR_FOLDER = True
+            elif key == '--start-from':
+                global INDEX_TO_START
+                MAX_IMAGES = int(MAX_IMAGES+int(value))
+                INDEX_TO_START = int(value)
     except getopt.GetoptError as err:
         # print help information and exit:
         print(str(err))  # will print something like "option -a not recognized"
         sys.exit(2)
 
-    run(max_images=max_images)
 
+def initalize_var_and_directory():
+    """
+        Method that creates a directory to place downloaded images
+    """
+    get_argv()
+    if CLEAR_FOLDER:
+        try:
+            shutil.rmtree(DOWNLOAD_FOLDER)
+        except FileNotFoundError as err:
+            print('The directory {} not exists in path, but don\'t worry, we will create for you'.format(DOWNLOAD_FOLDER.split('\\')[-1]))
+    # how validate if a folder exist on path 
+        # if not os.path.exists(DOWNLOAD_FOLDER): ...
+    try:
+        os.mkdir(DOWNLOAD_FOLDER)
+    except FileExistsError as err:
+        print(err)
+
+
+def move_downloads(*args):
+    img_name, img_url, page_title = args
+    print(img_url)
+    print(f'Downloading image from {page_title}...')
+    if not os.path.exists('{}\{}'.format(DOWNLOAD_FOLDER, img_name)):
+        urllib.request.urlretrieve(img_url, img_name) # To Download the image
+        shutil.move(img_name, DOWNLOAD_FOLDER)
+
+
+
+def run():
+    """
+        This method download all image from https://xkcd.com
+    """
+    initalize_var_and_directory()
+    for i in range(INDEX_TO_START, MAX_IMAGES+1):
+        response = requests.get(URL + str(i))
+        if response.status_code == 200:
+            try:
+                img_name, img_url, page_title = find_image(markup_content= response.content)
+            except ValueError as e:
+                print(e)
+            else:
+                move_downloads(img_name, img_url, page_title)
+            finally:
+                pass
+        else:
+            print(response.status_code)
+    print('Download completed')
+
+if __name__ == '__main__':
+    run()
